5816912dc3359afa5d11728f3fc1cc386c9c0174
Ayush <ayushsinha4343@gmail.com>
2026-05-08 09:33:03 +0530

    final fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://school-cms-zlmm.onrender.com",  
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
