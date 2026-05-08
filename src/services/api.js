abc36a54239d49d351dc1bb25e2f6533b41972b7
Ayush <ayushsinha4343@gmail.com>
2026-05-08 08:17:22 +0530

    changes3

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",   // ✅ FIXED
+  baseURL: "https://school-cms-zlmm.onrender.com",   // ✅ FIXED
 });
 
 api.interceptors.request.use((config) => {
